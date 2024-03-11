3a06c97c36f5d4c4f1b3d191f6cbfd5550df8236
FarrelEdric <franciscusfarrel14@gmail.com>
2024-03-11 11:19:25 +0700

    praktikum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,15 +7,59 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
+use function PHPUnit\Framework\returnValue;
+
 class UserController extends Controller
 {
     public function index()
     {
+        $user = UserModel::all();
+        return view('user',['data'=>$user]);
+
+
         // $user = UserModel::findOr(1, ['username', 'nama'], function () {
         //     abort(404);
         // });
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+
+        // $user = UserModel::where('level_id', 2)->count();
+
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::create([
+        //     'username'=>'manager11',
+        //     'nama'=>'Manager11',
+        //     'password'=>Hash::make('12345'),
+        //     'level_id'=>2,
+        // ]);
+        // $user->username = 'manager12';
+
+        // $user->save();
+        //  $user->isDirty();
+        //  $user->isDirty('username');
+        //  $user->isDirty('nama');
+        //  $user->isDirty(['nama','username']);
+
+        //  $user->isClean();
+        //  $user->isClean('username');
+        //  $user->isClean('nama');
+        //  $user->isClean(['nama','username']);
+        //  $user->isDirty();
+        //  $user->isClean();
+        //  dd($user->isDirty());
+
+        // $user->wasChanged();
+        // $user->wasChanged('username');
+        // $user->wasChanged(['username','level_id']);
+        // $user->wasChanged('nama');
+        // dd($user->wasChanged(['nama','username']));
+
+        // return view('user', ['data' => $user]);
+        
+    }
+    public function tambah()
+    {
+        return view('user_tambah');
     }
 }
